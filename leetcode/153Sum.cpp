370a45bfaa9ded3faaaeb69322a28429199fab0c
Shayan Alam <shamowo69@gmail.com>
2026-09-03 23:36:09 +0530

    15

---
+++ leetcode/153Sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin() , nums.end());
+        vector<vector<int>> ans;
+        for (int i = 0; i < nums.size() ; i++)
+        {
+            if ( i > 0 && nums[i] == nums[i-1])
+            continue;
+            int left = i + 1, right = nums.size() - 1;
+            while (left < right)
+            {
+            if ( nums[left] + nums[right] == (0 - nums[i]) )
+            {
+                ans.push_back({nums[i], nums[left], nums[right]});
+                left++; right--;
+
+                  while (left < right && nums[left] == nums[left - 1])
+                        left++;
+
+                  while (left < right && nums[right] == nums[right + 1])
+                        right--;
+            }
+            else if (nums[left] + nums[right] > (0 - nums[i]))
+            right--;
+            else if (nums[left] + nums[right] < (0 - nums[i]))
+            left++;
+            }
+        }
+        return ans;
+    }
+};
